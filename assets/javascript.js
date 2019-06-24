c6b4a85413c1543748977b19e32d9f2c15a5aeb0
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-23 19:45:27 -0600

    Minutes being captured

--- assets/javascript.js
+++ assets/javascript.js
@@ -44,21 +44,46 @@ $("#addTrainButton").on("click", function(event) {
 
 database.ref().on("child_added",function(childSnapshot){
     
-    //Storing train name in firebase data into variable
+    //Storing train name in firebase data into variables
     let trainName = childSnapshot.val().name;
     let destinationName = childSnapshot.val().destination;
     let frequencyTime = childSnapshot.val().frequency;
-    let time = childSnapshot.val().firstTrainTime;
+    let time = childSnapshot.val().time; //in military time for first train
+
+    //Current time
+    let currentTime = moment().format("HH:mm");
+    console.log(currentTime);
+
+    //Math for calculating next arrival using first train time and current time and frequency
     
+    //First Train vs currentTime minutes difference
+    let firstTrainTimeMinutes = moment.duration(time).minutes();
+    let currentTimeMinutes= moment.duration(currentTime).minutes();
+    let diff = Math.abs(firstTrainTimeMinutes - currentTimeMinutes); //absolute value
+    console.log("Minutes Difference: " + diff);
+
+    //Diff % frequency to get minutes for next train
+    let remainderMinutes = diff % frequencyTime;
+    console.log("Minutes till next train: " + remainderMinutes)
+
+    //Calculating next arrival 
+    let nextArrival = moment().add(remainderMinutes,"minutes")
+    console.log("NExt Train: " + moment(nextArrival).format("HH:mm"));
+
     //Create new row
-    let newRow = $("<tr>").append(
+     newRow = $("<tr>").append(
         $("<td>").text(trainName),$("<td>").text(destinationName),
-        $("<td>").text(frequencyTime)
-    );
+        $("<td>").text(frequencyTime));
+    
 
     //// Append the new row to the table
     $("#train-table > tbody").append(newRow);
+
+
+     
 });
+
+
 
 /* 
 // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
